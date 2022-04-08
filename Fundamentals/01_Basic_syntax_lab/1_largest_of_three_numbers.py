ced6f5b7c48e439cf28dac09bb819ec7c287c7d3
Кристиян Николов <102967190+kristiqnnikolov@users.noreply.github.com>
2022-04-08 18:42:27 +0300

    Update 1_largest_of_three_numbers.py

--- Fundamentals/01_Basic_syntax_lab/1_largest_of_three_numbers.py
+++ Fundamentals/01_Basic_syntax_lab/1_largest_of_three_numbers.py
@@ -1,5 +1,4 @@
 # Write a program that receives three whole numbers and prints the largest one.
-# Напишете програма, която получава три цели числа и отпечатва най-голямото.
 
 number1 = int(input())
 number2 = int(input())
