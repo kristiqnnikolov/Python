5e03ccf0e4d365b2e0614d95071a523a2958f9e5
Кристиян Николов <102967190+kristiqnnikolov@users.noreply.github.com>
2022-05-13 08:10:32 +0300

    Add files via upload

---
+++ Fundamentals/06_data_types_and_variables_more_exercises/1_exchange_integers.py
@@ -0,0 +1,11 @@
+# Read two integer numbers and, after that, exchange their values.
+# Print the variable values before and after the exchange, as shown below:
+
+a = int(input())
+b = int(input())
+print(f"Before:")
+print(f"a = {a}")
+print(f"b = {b}")
+print(f"After:")
+print(f"a = {b}")
+print(f"b = {a}")
