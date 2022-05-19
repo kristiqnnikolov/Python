a3dcbe88061e719c9ab586cde33c3c86ad1b4c8f
Кристиян Николов <102967190+kristiqnnikolov@users.noreply.github.com>
2022-05-19 07:48:40 +0300

    Add files via upload

---
+++ Fundamentals/08_lists_basics_exercise/04_number_beggars.py
@@ -0,0 +1,25 @@
+# You will receive 2 lines of input.
+# On the first line, you will receive a single string of integers, separated by a comma and a space ", ".
+# On the second line, you will receive a count of beggars.
+# Your job is to print a list with the sum of what each beggar brings home, assuming they all take regular turns,
+# from the first to the last number in the list.
+# For example: [1, 2, 3, 4, 5] for 2 beggars will return a result of 9 and 6, as the first one takes [1, 3, 5],
+# the second one collects [2, 4]. The same list with 3 beggars would produce a better outcome for the second beggar:
+# 5, 7 and 3, as they will respectively take [1, 4], [2, 5], and [3].
+# Also, note that not all beggars have to take the same amount of "offers",
+# meaning that the length of the list is not necessarily a multiple of n.
+# The list length could be even shorter - i.e., the last beggars will take nothing (0).
+
+numbers = input()
+beggars = int(input())
+numbers_as_string = numbers.split(", ")
+all = []
+start_index = 0
+for beggar in range(beggars):
+    total = 0
+    for i in range(start_index, len(numbers_as_string), beggars):
+        total += int(numbers_as_string[i])
+    all.append(total)
+    start_index += 1
+
+print(all)
