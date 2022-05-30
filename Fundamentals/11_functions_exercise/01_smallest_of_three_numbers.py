fbe2387cf341bad475da428144ff13cb6168cd75
Кристиян Николов <102967190+kristiqnnikolov@users.noreply.github.com>
2022-05-30 10:23:48 +0300

    Add files via upload

---
+++ Fundamentals/11_functions_exercise/01_smallest_of_three_numbers.py
@@ -0,0 +1,11 @@
+# Write a function that receives three integer numbers and returns the smallest. Print the result on the console.
+# Use an appropriate name for the function.
+
+def numbers(a, b, c, ):
+    return (min(a, b, c, ))
+
+
+number_1 = int(input())
+number_2 = int(input())
+number_3 = int(input())
+print(numbers(number_1, number_2, number_3))
