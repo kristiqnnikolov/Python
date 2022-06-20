f9663e737ecb249b12cb7fb3d60ee9834f856e39
Кристиян Николов <102967190+kristiqnnikolov@users.noreply.github.com>
2022-06-20 08:07:43 +0300

    Add files via upload

---
+++ Fundamentals/01 Basic Syntax - Lab/1_largest_of_three_numbers.py
@@ -0,0 +1,7 @@
+# Write a program that receives three whole numbers and prints the largest one.
+
+number1 = int(input())
+number2 = int(input())
+number3 = int(input())
+
+print(max(number1, number2, number3))
---
+++ Fundamentals/01 Basic Syntax - Lab/2_number_definer.py
@@ -0,0 +1,25 @@
+# Write a program that reads a floating-point number and:
+# -	prints "zero" if the number is zero.
+# -	prints "positive" or "negative".
+# -	adds "small" if the absolute value of the number is less than 1 and it is not 0, or "large" if it exceeds
+# 1 000 000.
+
+number = float(input())
+if number == 0:
+    print("zero")
+elif number > 0:
+    if 0 < number < 1:
+        print("small positive")
+    else:
+        if number > 1000000:
+            print("large positive")
+        else:
+            print("positive")
+elif number < 0:
+    if 0 > number > -1:
+        print("small negative")
+    else:
+        if number < -1000000:
+            print("large negative")
+        else:
+            print("negative")
