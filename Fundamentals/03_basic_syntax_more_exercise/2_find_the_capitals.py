3746da7e64b2b97ea477df830632f2c9b18c9339
Кристиян Николов <102967190+kristiqnnikolov@users.noreply.github.com>
2022-06-22 09:00:56 +0300

    Delete Fundamentals/03_basic_syntax_more_exercise directory

--- Fundamentals/03_basic_syntax_more_exercise/2_find_the_capitals.py
+++
@@ -1,8 +0,0 @@
-# Write a program that takes a single string and prints a list of all the capital letters indices.
-
-text = input()
-result = []
-for index in range(len(text)):
-    if text[index].isupper():
-        result.append(index)
-print(result)
