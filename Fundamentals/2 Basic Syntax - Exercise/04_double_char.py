aff1d678fa101c86df8af2da04e5d51354ce9082
Кристиян Николов <102967190+kristiqnnikolov@users.noreply.github.com>
2022-06-22 09:00:44 +0300

    Delete Fundamentals/2 Basic Syntax - Exercise directory

--- Fundamentals/2 Basic Syntax - Exercise/04_double_char.py
+++
@@ -1,9 +0,0 @@
-# You will be given a string.
-# You should print a string in which each character (case-sensitive) is repeated twice.
-
-
-text = input()
-result = ""
-for char in text:
-    result += char * 2
-print(result)
