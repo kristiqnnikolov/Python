306a7372813b888a6bf6fee80220be59419c5ba3
Кристиян Николов <102967190+kristiqnnikolov@users.noreply.github.com>
2022-06-24 11:04:32 +0300

    Delete Fundamentals/04_data_types_and_variables_lab directory

--- Fundamentals/04_data_types_and_variables_lab/4_convert_meters_to_kilometers.py
+++
@@ -1,6 +0,0 @@
-# You will be given an integer that represents a distance in meters.
-# Write a program that converts meters to kilometers formatted to the second decimal point.
-
-meters = float(input())
-kilometers = meters / 1000
-print(f"{kilometers:.2f}")
