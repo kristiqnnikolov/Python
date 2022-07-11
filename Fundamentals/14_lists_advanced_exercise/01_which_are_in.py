8b31e906c004b2f062dda89dc965aef754d28df3
Кристиян Николов <102967190+kristiqnnikolov@users.noreply.github.com>
2022-07-11 07:50:13 +0300

    Delete Fundamentals/14_lists_advanced_exercise directory

--- Fundamentals/14_lists_advanced_exercise/01_which_are_in.py
+++
@@ -1,8 +0,0 @@
-# You will be given two sequences of strings, separated by ", ".
-# Print a new list containing only the strings from the first input line,
-# which are substrings of any string in the second input line.
-
-words = input().split(", ")
-letters = input()
-new_list = [x for x in words if x in letters]
-print(new_list)
