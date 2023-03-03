d2d10a8c3eead75d0b360f7be359b2b61aca78d4
Кристиян Николов <102967190+kristiqnnikolov@users.noreply.github.com>
2023-03-03 14:00:36 +0200

    Add files via upload

---
+++ Advanced/06 Multidimensional Lists - Lab/3_flattening_matrix.py
@@ -0,0 +1,11 @@
+# Write a program that receives a matrix and prints the flattened version of it (a list with all the values).
+# For example, the flattened list of the matrix: [[1, 2], [3, 4]] will be [1, 2, 3, 4].
+# On the first line, you will receive the number of a matrix's rows.
+# On the next rows, you will get the elements for each column separated with a comma and a space ", ".
+
+matrix = []
+
+for none in range(int(input())):
+    numbers = [int(x) for x in input().split(', ')]
+    matrix.extend(numbers)
+print(matrix)
